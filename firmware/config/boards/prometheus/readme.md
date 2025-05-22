28dc3c132cef35ca96f0aff4e0e820c31cab2c46
Bruno Ferreira <126033267+gerefi@users.noreply.github.com>
2025-05-22 23:11:12 +0200

    Convert

--- firmware/config/boards/prometheus/readme.md
+++ firmware/config/boards/prometheus/readme.md
@@ -4,4 +4,4 @@ Prometheus board.
 
 http://gerefi.com/forum/viewtopic.php?f=4&t=1215
 
-https://github.com/andreika-git/prometheus
+https://github.com/BrunoFA-git/prometheus
--- misc/git_scripts/git_add_upstream.bat
+++ misc/git_scripts/git_add_upstream.bat
@@ -2,7 +2,7 @@ git remote -v
 git remote add upstream https://github.com/gerefi/gerefi.git
 
 rem The heros, in alpabetical order
-git remote add andreika-git https://github.com/andreika-git/gerefi
+git remote add BrunoFA-git https://github.com/BrunoFA-git/gerefi
 git remote add DonaldBecker https://github.com/DonaldBecker/gerefi
 git remote add dron0gus     https://github.com/dron0gus/gerefi 
 git remote add mck1117      https://github.com/mck1117/gerefi 
